b8b149f94099ad6cf6c924abe4a8f6cd7ebf7cc4
Yuri Mel0 <meloy316@gmail.com>
2022-05-22 03:42:08 -0300

    adicionando exercicio de poo em java - metodos

---
+++ src/com/yuri/poojava/aula02/labs/Lampada.java
@@ -0,0 +1,18 @@
+package com.yuri.poojava.aula02.labs;
+
+public class Lampada {
+	String marca;
+	String cor;
+	String tecnologiaIluminacao;
+	String forma;
+	double potencia;
+	double preco;
+	
+	void ligarLampada() {
+		System.out.println("Lampada ligada!");
+	}
+	
+	void desligarLampada() {
+		System.out.println("Lampada Desligada!");
+	}
+}
---
+++ src/com/yuri/poojava/aula02/labs/TesteLabs.java
@@ -0,0 +1,14 @@
+package com.yuri.poojava.aula02.labs;
+
+public class TesteLabs {
+
+	public static void main(String[] args) {
+		
+		Lampada lampada = new Lampada();
+		
+		lampada.ligarLampada();
+		lampada.desligarLampada();
+
+	}
+
+}
